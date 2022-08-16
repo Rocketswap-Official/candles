e66bf6cbb9ddb64c7ac125848ca1ea64d8e7dc6a
cloakdkilla <getrektwithme1@gmail.com>
2022-08-16 13:56:54 +0100

    update config

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const config_prod = {
     app_name: "Rocketswap",
     amm_contract: process.env.CONTRACT_NAME || "con_rocketswap_official_v1_1",
     network_type: process.env.NETWORK_TYPE || "mainnet",
-    block_service_urls: process.env.block_service_urls?.split(",") || ["165.22.47.195:3535"]
+    block_service_urls: process.env.block_service_urls?.split(",") || ["0.0.0.0:3535"]
 };
 
 export const getConfig = () => config_prod
