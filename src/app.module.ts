64c22135a4ddad75c0c7195c04cc5523739bc431
cloakdkilla <getrektwithme1@gmail.com>
2022-08-16 14:09:33 +0100

    db imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LastBlockEntity } from './entities/last-block.entity';
+import { PairEntity } from './entities/pair.entity';
+import { TokenEntity } from './entities/token.entity';
+import { TradeHistoryEntity } from './entities/trade-history.entity';
 import { TauCandlesService } from './services/tau-candles.service';
-import { TauCandleEntity } from './entities/tau-candles.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [TauCandleEntity],
+      entities: [LastBlockEntity, PairEntity, TokenEntity, TradeHistoryEntity],
       synchronize: true,
       autoLoadEntities: true
     }),
@@ -18,3 +21,4 @@ import { TauCandleEntity } from './entities/tau-candles.entity';
   providers: [AppService, TauCandlesService],
 })
 export class AppModule { }
+ 
