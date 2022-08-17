ac6f7f3f35993f8430d5dda078b07989a4d1e2b7
cloakdkilla <getrektwithme1@gmail.com>
2022-08-18 00:58:52 +0100

    update config

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,18 @@
+import { T_Resolution } from "./types";
+
 export const config_prod = {
     app_name: "Rocketswap",
-    amm_contract: process.env.CONTRACT_NAME || "con_rocketswap_official_v1_1",
-    network_type: process.env.NETWORK_TYPE || "mainnet",
+    amm_contract: "con_rocketswap_official_v1_1",
+    amm_native_token: "con_rswp_lst001",
+    network_type: "mainnet",
     block_service_urls: process.env.block_service_urls?.split(",") || ["0.0.0.0:3535"]
 };
+
+/**
+ * The candle resolutions we want to create
+ */
+
+export const candle_resolutions: T_Resolution[] = ["1h", "4h", "1d"]
 
 export const getConfig = () => config_prod
 
